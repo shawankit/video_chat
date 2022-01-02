3f13bc37723c6054ee5a392380c239e847e686df
Ankit Shaw <ankit.shaw@keross.com>
2022-01-02 20:48:19 +0530

    Commit Changes 3

--- commands.sh
+++ commands.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-npm build
-npm start
+echo "Starting"
+docker rmi nodeimage;
+docker stop nodecontainer;
+docker rm nodecontainer;
+docker build -t nodeimage .;
+docker run -d --name nodecontainer -p 5000:5000 nodeimage;
+echo "Done"
